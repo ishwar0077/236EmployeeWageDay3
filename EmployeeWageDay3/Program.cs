f00328be2646ea4db640ccfa34a7911bb097caed
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-19 11:58:19 +0530

    Greeting message

---
+++ EmployeeWageDay3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageDay3
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welocome to Employee wage Program");
+         
+        
+        }
+    }
+}
